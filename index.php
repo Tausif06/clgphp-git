e2db783ed27e4e9272904d4b97f3910e4eab94d0
Tausif <tkuravle@gmail.com>
2024-01-08 19:08:17 +0530

    it is creted

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>php project</title>
+        
+        <link rel="stylesheet" href="style.css" type="text/css">
+
+    </head>
+    <title> home </title>
+   
+    <body>
+        <section class="main">
+       
+       <nav>
+       
+           <a href="#" class="logo">
+            <img src="logo.jpg" height="50" >
+            </a>
+
+        <ul class="menu">
+            <li><a href="">Home</a> </li>
+            <li><a href="">contact</a> </li>
+            <li><a href="">orders</a> </li>
+            <li><a href="login.html">login</a> </li>
+        </ul>
+       </nav>
+        <div class="section1">
+            <label class="imgtext">we deliver best tiffin with great recepie  </label>
+            <img class="mainimg" src="homeimg.webp">
+        </div>
+        </section>
+    </body>
+</html>
